ccd4ddc029a9b0f67f4d4ec4ed6c30492589457d
Mónica Pastor <mpast@users.noreply.github.com>
2024-09-04 15:20:54 +0200

    Update aws.py

--- aws.py
+++ aws.py
@@ -9,3 +9,5 @@ import pymongo
 def aws_upload(data: Dict):
     database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
     database.push(data)
+    database = aws_lib.connect("AKIAF4SADJFR45BAWSZ9", "hjshnk5ex5u32365AWS354/JKGjhz545d89sldjl")
+    database.push(data)
